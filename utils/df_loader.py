6cbb57e83e50ab2edbff3ba40b570f67ba704666
Chihcheng Hsieh <88370494+Cuzoex@users.noreply.github.com>
2021-08-15 07:37:42 +1000

    add compas.

--- utils/df_loader.py
+++ utils/df_loader.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from utils.preprocessing import get_columns_type, transform_to_dummy, label_encode, remove_missing_values
 
+import numpy as np
 
 def load_adult_df():
     ##### Pre-defined #####
@@ -31,6 +32,53 @@ def load_german_df():
     df = pd.read_csv('./datasets/german.csv',delimiter=',', skipinitialspace=True)
 
     df[target_name] = df[target_name].apply(lambda x: "Y" if x==1 else "N")
+
+    feature_names = [col for col in df.columns if col != target_name]
+
+    df = remove_missing_values(df)
+
+    possible_outcomes = list(df[target_name].unique())
+
+    numerical_cols, categorical_cols, columns_type = get_columns_type(df)
+
+    return df, feature_names, numerical_cols, categorical_cols, columns_type, target_name, possible_outcomes
+
+def load_compas_df():
+    
+    target_name = 'class'
+
+    df = pd.read_csv('./datasets/COMPAS.csv',delimiter=',', skipinitialspace=True)
+
+    columns = ['age', 'age_cat', 'sex', 'race',  'priors_count', 'days_b_screening_arrest', 'c_jail_in', 'c_jail_out',
+               'c_charge_degree', 'is_recid', 'is_violent_recid', 'two_year_recid', 'decile_score', 'score_text']
+
+    df = df[columns]
+
+    df['days_b_screening_arrest'] = np.abs(df['days_b_screening_arrest'])
+
+    df['c_jail_out'] = pd.to_datetime(df['c_jail_out'])
+    df['c_jail_in'] = pd.to_datetime(df['c_jail_in'])
+    df['length_of_stay'] = (df['c_jail_out'] - df['c_jail_in']).dt.days
+    df['length_of_stay'] = np.abs(df['length_of_stay'])
+
+    df['length_of_stay'].fillna(df['length_of_stay'].value_counts().index[0], inplace=True)
+    df['days_b_screening_arrest'].fillna(df['days_b_screening_arrest'].value_counts().index[0], inplace=True)
+
+    df['length_of_stay'] = df['length_of_stay'].astype(int)
+    df['days_b_screening_arrest'] = df['days_b_screening_arrest'].astype(int)
+
+    def get_class(x):
+        if x < 7:
+            return 'Medium-Low'
+        else:
+            return 'High'
+
+    df['class'] = df['decile_score'].apply(get_class)
+
+    del df['c_jail_in']
+    del df['c_jail_out']
+    del df['decile_score']
+    del df['score_text']
 
     feature_names = [col for col in df.columns if col != target_name]
 
