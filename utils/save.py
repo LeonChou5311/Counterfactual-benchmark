1e0e7fd70c4618864863141f0fdd928ea3cb5f53
Chihcheng Hsieh <88370494+Cuzoex@users.noreply.github.com>
2021-08-16 07:55:41 +1000

    print save path.

--- utils/save.py
+++ utils/save.py
@@ -1,7 +1,10 @@
 import os
 
 def save_result_as_csv(cf_alg_name, dataset_name, results_df):
-    os.makedirs(f"./results/{cf_alg_name}_{dataset_name}", exist_ok=True)
+    path = f"./results/{cf_alg_name}_{dataset_name}"
+    os.makedirs(path, exist_ok=True)
     for df_k in results_df.keys():
-        results_df[df_k].to_csv(f"./results/{cf_alg_name}_{dataset_name}/{cf_alg_name}_{dataset_name}_{df_k}_result.csv")
+        results_df[df_k].to_csv(f"{path}/{cf_alg_name}_{dataset_name}_{df_k}_result.csv")
+
+    print(f"Result has been saved to {path}")
     
