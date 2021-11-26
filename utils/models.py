a7a3117285440461befdd38965456d69ce704c5e
Leon Chou <l123159659@gmail.com>
2021-11-26 19:40:42 +1000

    init.

--- utils/models.py
+++ utils/models.py
@@ -38,6 +38,36 @@ def train_three_models(X_train, y_train):
     return models
 
 
+
+def train_three_models_lp(X_train, y_train):
+    '''
+    Construct and train ['dt', 'rfc', 'nn']
+
+    ---
+    Return -> A dictionary container three trained models.
+    '''
+    nn = tf.keras.models.Sequential(
+            [
+                tf.keras.layers.Dense(24,activation='relu'),
+                tf.keras.layers.Dense(12,activation='relu'),
+                tf.keras.layers.Dense(1),
+                tf.keras.layers.Activation(tf.nn.sigmoid),
+            ]
+        )
+    nn.compile(optimizer="Adam", loss='binary_crossentropy', metrics=['accuracy'])
+    nn.fit(X_train, y_train, batch_size=64, epochs=5, shuffle=True)
+
+    models = {
+        "dt": DecisionTreeClassifier(min_sample_leaf=10,max_depths=10).fit(X_train,y_train),
+        "rfc": RandomForestClassifier(n_estimators=20, min_sample_leaf=10,max_depths=10).fit(X_train,y_train),
+        "nn": nn,
+    }
+
+
+    return models
+
+
+
 def evaluation_test(models, X_test, y_test):
     '''
     Evaluation the trained models.
@@ -60,6 +90,17 @@ def save_three_models(models, dataset_name, path='./saved_models'):
     pickle.dump(models['rfc'], open(f'{storing_folder}/rfc.p', 'wb'))
     models['nn'].save(f'{storing_folder}/nn.h5',overwrite=True)
 
+def save_lp_three_models(models, dataset_name, path='./saved_models'):
+    '''
+    Save three trained models to desired `path`.
+    '''
+    storing_folder= f'{path}/{dataset_name}'
+    os.makedirs(storing_folder, exist_ok=True)
+
+    pickle.dump(models['dt'], open(f'{storing_folder}/dt_lp.p', 'wb'))
+    pickle.dump(models['rfc'], open(f'{storing_folder}/rfc_lp.p', 'wb'))
+    models['nn'].save(f'{storing_folder}/nn_lp.h5',overwrite=True)
+
 
 def load_three_models(num_features, dataset_name, path='./saved_models'):
     '''
@@ -73,6 +114,24 @@ def load_three_models(num_features, dataset_name, path='./saved_models'):
     models['dt'] = pickle.load(open(f'{storing_folder}/dt.p', 'rb'))
     models['rfc'] = pickle.load(open(f'{storing_folder}/rfc.p', 'rb'))
     models['nn'] = tf.keras.models.load_model(f'{storing_folder}/nn.h5')
+
+    ## Initialise NN output shape as (None, 1) for tensorflow.v1
+    models['nn'].predict(np.zeros((2, num_features)))
+
+    return models
+
+def load_lp_three_models(num_features, dataset_name, path='./saved_models'):
+    '''
+    Load pre-trained model from the `path`.  Will be saved in `./saved_models` by default
+    '''
+
+    storing_folder= f'{path}/{dataset_name}'
+
+    ### Load
+    models = {}
+    models['dt'] = pickle.load(open(f'{storing_folder}/dt_lp.p', 'rb'))
+    models['rfc'] = pickle.load(open(f'{storing_folder}/rfc_lp.p', 'rb'))
+    models['nn'] = tf.keras.models.load_model(f'{storing_folder}/nn_lp.h5')
 
     ## Initialise NN output shape as (None, 1) for tensorflow.v1
     models['nn'].predict(np.zeros((2, num_features)))
